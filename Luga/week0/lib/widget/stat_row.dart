0c1af007105269685c42a9a3becaee8fb73cbf8c
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-09-19 07:08:23 +0900

    fix: 카드 사이 간격에 margin을 적용하였기 때문에 아이템 생성 시 padding 값 제거

--- Luga/week0/lib/widget/stat_row.dart
+++ Luga/week0/lib/widget/stat_row.dart
@@ -9,25 +9,10 @@ class StatRow extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
-      children: const[
-        Expanded(
-            child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 4),
-                child: StatItem(label: '본 영화', value: '342'),
-            ),
-        ),
-        Expanded(
-            child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 4),
-                child: StatItem(label: '평점', value: '4.2'),
-            ),
-        ),
-        Expanded(
-            child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 4),
-                child: StatItem(label: '즐겨찾기', value: '58'),
-            ),
-        ),
+      children: const [
+        Expanded(child: StatItem(label: '본 영화', value: '342')),
+        Expanded(child: StatItem(label: '평점', value: '4.2')),
+        Expanded(child: StatItem(label: '즐겨찾기', value: '58')),
       ],
     );
   }
