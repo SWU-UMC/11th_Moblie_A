2e67875ae29f794c354986331ff5f1efb0c89b3f
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-09-19 06:32:38 +0900

    fix: 장르 칩 import 오타 수정 및 프로필 이미지 사진 변경

--- Luga/week0/lib/widget/profile_header.dart
+++ Luga/week0/lib/widget/profile_header.dart
@@ -22,7 +22,7 @@ class ProfileHeader extends StatelessWidget {
 
             child: ClipOval(
                 child: Image.asset(
-                    'assets/images/profile.png',
+                    'assets/images/profile/profile_movielog.png',
                     fit: BoxFit.cover,
                 ),
             ),
