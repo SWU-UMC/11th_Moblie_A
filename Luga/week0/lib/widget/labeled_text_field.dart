949061d5c73a8f24e30e01445fd88b1b09edcb7a
ochyeon <136903107+ochyeon@users.noreply.github.com>
2026-09-21 15:28:59 +0900

    feat: 입력창 재사용 가능하도록 widget으로 분리 구현

---
+++ Luga/week0/lib/widget/labeled_text_field.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LabeledTextField extends StatelessWidget {
+    const LabeledTextField({
+        super.key,
+        required this.controller,
+        required this.labelText,
+        required this.hintText,
+        required this.validator,
+        this.focusNode,
+        this.nextFocusNode,
+        this.obscureText = false,
+        this.keyboardType,
+        this.textInputAction = TextInputAction.next,
+        this.onChanged,
+    });
+
+    final TextEditingController controller;
+    final String labelText;
+    final String hintText;
+    final String? Function(String?) validator;
+    final FocusNode? focusNode;
+    final FocusNode? nextFocusNode;
+    final bool obscureText;
+    final TextInputType? keyboardType;
+    final TextInputAction textInputAction;
+    final ValueChanged<String>? onChanged;
+
+    @override
+    Widget build(BuildContext context) {
+        return TextFormField(
+            consroller: controller,
+            focusNode: focusNode,
+            obscureText: obscureText,
+            keyboardType: keyboardType,
+            textInputAction: textInputAction,
+            decoration: InputDecoration(
+                labelText: labelText,
+                hintText: hintText,
+            ),
+            validator: validator,
+            onChanged: onChanged,
+            onFieldSubmitted: (_) {
+                if (nextFocusNode != null) {
+                    FocusScope.of(context).requestFocus(nextFocusNode);
+                } else {
+                    FocusScope.of(context).unfocus();
+                }
+            },
+        );
+    }
+}
